7ca853fa20d9ea97ad80a127a3b40d07d442be0e
camachon18 <camachon18@up.edu>
2015-12-09 13:22:10 -0800

    AI almost done!!!!- Nate

--- app/src/main/java/com/example/administrator/battleship/BattleshipComputerPlayer2.java
+++ app/src/main/java/com/example/administrator/battleship/BattleshipComputerPlayer2.java
@@ -8,14 +8,16 @@ public class BattleshipComputerPlayer2 {
     private int col;
     private int lastHitRow;
     private int lastHitCol;
-    private int targetRow;
-    private int targetCol;
+    private int firstHitRow;
+    private int firstHitCol;
+    private boolean stillOnTarget;
     private boolean huntMode;
     private boolean killMode;
 
     public BattleshipComputerPlayer2() {
         huntMode = true;
         killMode = false;
+        stillOnTarget = false;
     }
 
     public void fire(BattleshipGameState state) {
@@ -60,7 +62,54 @@ public class BattleshipComputerPlayer2 {
                     break;
                 }
             }
+            int j = 0;
+            while (state.getPlayerID() == 1) {
+                if (j == 0) {//fire at right adjacent
+                    row = firstHitRow;
+                    col = firstHitCol + 1;
+                    if (col > 9){//out of bounds on right
+                        col = firstHitCol - 1;//change to left of hit
+                    }
+                    state.shipHit(row, col, 0);
+                }
+                else if (j == 1) {//fire at left adjacent
+                    row = firstHitRow;
+                    col = firstHitCol - 1;
+                    if (col < 0) {//out of bounds on left
+                        row = firstHitRow - 1;//change to fire at top of hit
+                        col = firstHitCol;
+                    }
+                    state.shipHit(row, col, 0);
+                }
+                else if (j == 2) {//fire at top adjacent
+                    row = firstHitRow - 1;
+                    col = firstHitCol;
+                    if (row < 0) {//out of bounds on top
+                        row = firstHitRow + 1;//change to fire below hit
+                    }
+                    state.shipHit(row, col, 0);
+                }
+                if (j == 3) {//fire at bottom adjacent
+                    row = firstHitRow + 1;
+                    col = firstHitCol;
+                    if (row > 9) {//out of bounds on bottom
+                        break; //no spots left around hit;
+                    }
+                    state.shipHit(row, col, 0);
+                }
+                j++;
+                if (j > 3){
+                    break;
+                }
+            }
             if (state.getPlayerID() == 1){//if all spots around hit were checked
+                if (firstHitCol != lastHitCol && firstHitRow != lastHitRow) {
+                    firstHitRow = lastHitRow;
+                    firstHitCol = lastHitCol;
+                }
+                else {
+                    stillOnTarget = false;
+                }
                 while (state.getPlayerID() == 1) {
                     row = (int) (Math.random() * 10);
                     col = (int) (Math.random() * 10);
@@ -80,11 +129,21 @@ public class BattleshipComputerPlayer2 {
             killMode = true;
             lastHitRow = row;
             lastHitCol = col;
+            if (stillOnTarget == false) {
+                stillOnTarget = true;
+                firstHitRow = row;
+                firstHitCol = col;
+            }
 
         }
         else {
-            huntMode = true;
-            killMode = false;
+            if (stillOnTarget) {
+                huntMode = false;
+                killMode = true;
+            } else {
+                huntMode = true;
+                killMode = false;
+            }
         }
 
     }
